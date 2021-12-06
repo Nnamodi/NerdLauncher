e5d4ee2c74e58b409518f2ffe66740eb9282a854
Roland <nnamdiigede11@gmail.com>
2021-12-06 19:32:35 +0100

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherActivity.kt
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.nerdlauncher
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "NerdLauncherActivities"
+
+class NerdLauncherActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nerd_launcher)
+        recyclerView = findViewById(R.id.app_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        setupAdapter()
+    }
+
+    private fun setupAdapter() {
+        val startupIntent = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+        val activities = packageManager.queryIntentActivities(startupIntent, 0)
+        Log.i(TAG, "Found ${activities.size} activities")
+    }
+}
